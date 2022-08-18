815b5b468456bffb47316254329ad7fa1f129af6
adrian-soomro <adrian.soomro@gmail.com>
2022-08-18 08:18:07 +0100

    add statusline

--- README.md
+++ README.md
@@ -28,5 +28,4 @@ With regards to the [font requirement](#Miscelaneous) - the fonts need to be ins
 - lsp saga - code actions
 - telescope-repos - loads git repos to telescope
 - mfussenegger/nvim-dap - debugger
-- galaxyline/lualine - status line
 - glepnir- dashboard
--- lua/packer-config/init.lua
+++ lua/packer-config/init.lua
@@ -52,4 +52,13 @@ return require'packer'.startup(function()
       require('nvim-autopairs').setup({})
     end
   }
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+    config = function ()
+      require('lualine').setup({
+        options = { theme = 'dracula'}
+      })
+    end
+  }
 end)
