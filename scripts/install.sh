59a389ac220fea6ded58a24372fa5460eaeae526
adrian-soomro <adrian.soomro@gmail.com>
2023-08-16 21:44:54 +0100

    do an install, then sync

--- scripts/install.sh
+++ scripts/install.sh
@@ -58,6 +58,7 @@ function installNoobVim() {
 
   mv NoobVim "$pathToConfigDirectory"/nvim
 
+  nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerInstall' -u "$HOME/.config/nvim/lua/packer-config/init.lua"
   nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync' -u "$HOME/.config/nvim/lua/packer-config/init.lua"
   nvim --headless -c 'PackerSync' -u "$HOME/.config/nvim/lua/packer-config/init.lua"
 }
