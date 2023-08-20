dd11a36703999186211e547450edcec2257075bd
adrian-soomro <adrian.soomro@gmail.com>
2023-08-20 14:04:57 +0100

    try to enable autoclose on html tags

--- lua/tree-sitter-config/init.lua
+++ lua/tree-sitter-config/init.lua
@@ -3,10 +3,13 @@ require('nvim-treesitter.configs').setup {
   sync_install = false,
   highlight = {
     enable = true,
-    disable = { },
+    disable = {},
     additional_vim_regex_highlighting = false,
   },
   playground = {
+    enable = true
+  },
+  autotag = {
     enable = true
   }
 }
