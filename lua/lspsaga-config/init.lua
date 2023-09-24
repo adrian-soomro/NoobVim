c008c5f6addfe0703b4e17ebcc1980f70040e53b
Adrian Soomro <adrian.soomro@gmail.com>
2023-09-24 18:28:03 +0100

    updte lspsaga and change defaults

--- lua/lspsaga-config/init.lua
+++ lua/lspsaga-config/init.lua
@@ -1,66 +1,71 @@
 require('lspsaga').setup({
-  finder = {
-    edit = "<cr>",
-    vsplit = "v",
-    split = "i",
-    tabe = "t",
-    quit = "<esc>"
-  },
-  code_action = {
-    num_shortcut = true,
-    keys = {
-      quit = "<esc>",
-      exec = "<cr>"
-    }
-  },
-  diagnostic = {
-    show_code_action = true,
-    show_source = true,
-    jump_num_shortcut = true,
-    custom_fix = nil,
-    custom_msg = nil,
-    keys = {
-      exec_action = "<cr>",
-      quit = "<esc>",
-      go_action = "g"
-    },
-  },
-  rename = {
-    quit = "<esc>",
-    exec = "<CR>",
-    mark = "x",
-    confirm = "<CR>",
-    in_select = true,
-  },
-  outline = {
-    keys = {
-      jump = "o",
-      expand_collapse = "u",
-      quit = "<esc>",
-    },
-  },
-  symbol_in_winbar = {
-    enable = true,
-    separator = "  ",
-    hide_keyword = true,
-    show_file = true,
-    folder_level = 2,
-    respect_root = false,
-    color_mode = true,
-  },
+	finder = {
+		default = 'tyd+ref+imp+def',
+		keys = {
+			edit = "e",
+      toggle_or_open = "<cr>",
+			vsplit = "v",
+			split = "s",
+			tabe = "t",
+			tabnew = "r",
+			quit = "<esc>"
+		},
+	},
+	code_action = {
+		num_shortcut = true,
+		keys = {
+			quit = "<esc>",
+			exec = "<cr>"
+		}
+	},
+	diagnostic = {
+		show_code_action = true,
+		show_source = true,
+		jump_num_shortcut = true,
+		custom_fix = nil,
+		custom_msg = nil,
+		keys = {
+			exec_action = "<cr>",
+			quit = "<esc>",
+			go_action = "g"
+		},
+	},
+	rename = {
+		quit = "<esc>",
+		exec = "<CR>",
+		mark = "x",
+		confirm = "<CR>",
+		in_select = true,
+	},
+	outline = {
+		keys = {
+			jump = "o",
+			expand_collapse = "u",
+			quit = "<esc>",
+		},
+	},
+	symbol_in_winbar = {
+		enable = true,
+		separator = "  ",
+		hide_keyword = true,
+		show_file = true,
+		folder_level = 2,
+		respect_root = false,
+		color_mode = true,
+	},
 })
 
 vim.api.nvim_create_autocmd("CursorHold", {
-  buffer = bufnr,
-  callback = function()
-    local opts = {
-      focusable = false,
-      close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
-      border = 'rounded',
-      source = 'always',
-      prefix = ' ',
-      scope = 'cursor',
-    }
-    vim.diagnostic.open_float(nil, opts)
-  end
+	buffer = bufnr,
+	callback = function()
+		local opts = {
+			focusable = false,
+			close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+			border = 'rounded',
+			source = 'always',
+			prefix = ' ',
+			scope = 'cursor',
+		}
+		vim.diagnostic.open_float(nil, opts)
+	end
 })
--- lua/whichkey-config/init.lua
+++ lua/whichkey-config/init.lua
@@ -5,22 +5,22 @@ local utils = require('lua-utils')
 
 wk.setup({
   plugins = {
-    marks = true, -- shows a list of your marks on ' and `
-    registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+    marks = true,       -- shows a list of your marks on ' and `
+    registers = true,   -- shows your registers on " in NORMAL or <C-r> in INSERT mode
     spelling = {
-      enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+      enabled = true,   -- enabling this will show WhichKey when pressing z= to select spelling suggestions
       suggestions = 20, -- how many suggestions should be shown in the list?
     },
     -- the presets plugin, adds help for a bunch of default keybindings in Neovim
     -- No actual key bindings are created
     presets = {
-      operators = false, -- adds help for operators like d, y, ... and registers them for motion / text object completion
-      motions = false, -- adds help for motions
+      operators = false,   -- adds help for operators like d, y, ... and registers them for motion / text object completion
+      motions = false,     -- adds help for motions
       text_objects = true, -- help for text objects triggered after entering an operator
-      windows = true, -- default bindings on <c-w>
-      nav = true, -- misc bindings to work with windows
-      z = true, -- bindings for folds, spelling and others prefixed with z
-      g = true, -- bindings for prefixed with g
+      windows = true,      -- default bindings on <c-w>
+      nav = true,          -- misc bindings to work with windows
+      z = true,            -- bindings for folds, spelling and others prefixed with z
+      g = true,            -- bindings for prefixed with g
     },
   },
   -- add operators that will trigger motion and text object completion
@@ -41,25 +41,25 @@ wk.setup({
   },
   popup_mappings = {
     scroll_down = '<c-d>', -- binding to scroll down inside the popup
-    scroll_up = '<c-u>', -- binding to scroll up inside the popup
+    scroll_up = '<c-u>',   -- binding to scroll up inside the popup
   },
   window = {
-    border = "double", -- none, single, double, shadow
-    position = "bottom", -- bottom, top
-    margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
+    border = "double",        -- none, single, double, shadow
+    position = "bottom",      -- bottom, top
+    margin = { 1, 0, 1, 0 },  -- extra window margin [top, right, bottom, left]
     padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
     winblend = 0
   },
   layout = {
-    height = { min = 4, max = 25 }, -- min and max height of the columns
-    width = { min = 20, max = 50 }, -- min and max width of the columns
-    spacing = 3, -- spacing between columns
-    align = "left", -- align columns left, center or right
+    height = { min = 4, max = 25 },                                                     -- min and max height of the columns
+    width = { min = 20, max = 50 },                                                     -- min and max width of the columns
+    spacing = 3,                                                                        -- spacing between columns
+    align = "left",                                                                     -- align columns left, center or right
   },
-  ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
+  ignore_missing = false,                                                               -- enable this to hide mappings for which you didn't specify a label
   hidden = { "<silent>", "<cmd>", "<Cmd>", "<cr>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
-  show_help = true, -- show help message on the command line when the popup is visible
-  triggers = "auto", -- automatically setup triggers
+  show_help = true,                                                                     -- show help message on the command line when the popup is visible
+  triggers = "auto",                                                                    -- automatically setup triggers
   -- triggers = {"<leader>"} -- or specify a list manually
   triggers_blacklist = {
     -- list of mode / prefixes that should never be hooked by WhichKey
@@ -85,7 +85,7 @@ local leader_mapping = {
   g = {
     name = "Go to",
     d = { "<cmd>Lspsaga peek_definition<cr>", "Peek definition" },
-    i = { "<cmd>Lspsaga lsp_finder<cr>", "Check out the implementations" },
+    i = { "<cmd>Lspsaga finder<cr>", "Check out the implementations" },
     s = { "<cmd>Lspsaga hover_doc<cr>", "Check out the signature" },
   },
   c = {
