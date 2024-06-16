1d0c3e218d1a9051e2b0f8effff6484a68aeaf84
adrian-soomro <adrian.soomro@gmail.com>
2024-06-16 20:27:59 +0100

    dont reap, sleep inside headless instead

--- scripts/noobvim-installer.sh
+++ scripts/noobvim-installer.sh
@@ -2,6 +2,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 function installNoobVim() {
   source "$HOME/.profile"
+  pushd "$SCRIPT_DIR"
   git clone https://github.com/adrian-soomro/NoobVim.git
   pathToConfigDirectory="$HOME/.config"
   if [ ! -d "$pathToConfigDirectory" ]; then
@@ -9,6 +10,7 @@ function installNoobVim() {
   fi 
 
   mv NoobVim "$pathToConfigDirectory"/nvim
+  popd
   nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerInstall' -u "$HOME/.config/nvim/lua/packer-config/init.lua"
   nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync' -u "$HOME/.config/nvim/lua/packer-config/init.lua"
 }
@@ -24,19 +26,9 @@ function tidyUp() {
   source "$HOME/.profile"
   nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerInstall' -u "$HOME/.config/nvim/lua/packer-config/init.lua"
   nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync' -u "$HOME/.config/nvim/lua/packer-config/init.lua"
-  nvim --headless -E +'TSUpdateSync' +'quit'
-   rm -rf "$SCRIPT_DIR/tmp"
-   rm -rf "$SCRIPT_DIR/go"
-}
-
-function startReaper() {
-  sleep 450 
-  pid=$(ps aux | grep "nvim setup" | grep -v 'grep' | awk '{print $2}')
-  kill "$pid" 
+  nvim --headless -c 'TSUpdateSync' -c 'sleep 450' -c 'quit'
 }
 
 installNoobVim
 installAdditionalDependenciesWithMason
 tidyUp
-startReaper & 
-nvim setup
