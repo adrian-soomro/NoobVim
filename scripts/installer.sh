a7c929d522888570f89ad468d6cc853b142cf623
Adrian Soomro <37105695+adrian-soomro@users.noreply.github.com>
2024-06-16 20:30:20 +0100

    Feature/improve install process (#26)
    * dont reap, sleep inside headless instead
    
    * tidy up
    
    * remove extra space

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -3,5 +3,5 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo "Getting dependencies"
 "$SCRIPT_DIR/dependency-installer.sh"
 
-echo "installing NoobVim"
+echo "Installing NoobVim"
 "$SCRIPT_DIR/noobvim-installer.sh"
